a88586fc21ac58d108331a83ff662be0368d59b4
Kodjo Akama <johnakama32@gmail.com>
2026-01-15 17:57:11 +0000

    progress update

--- emojireaction/script.js
+++ emojireaction/script.js
@@ -1,13 +1,16 @@
-const happyBtn = document.querySelector('#happy-btn');
-
-happyBtn.addEventListener('click', () => {
-    updateCount(happyBtn);
-});
-
-function updateCount(buttonEl) {
-    const countEl = buttonEl.querySelector('.count');
+function updateCount(btn) {
+    const countEl = btn.querySelector('.count');
     let currCount = +countEl.textContent.split('/')[0];
+
     if (currCount === 10) return;
+
     currCount++;
+
     countEl.textContent = `${currCount}/10`;
 }
+const btns = document.querySelectorAll('.emoji-btn');
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        updateCount(btn);
+    });
+});
