5d823b3f0a13dd365b920944134ef521b452aba7
Kodjo Akama <johnakama32@gmail.com>
2026-01-28 16:07:00 +0000

    progress update

--- game/script.js
+++ game/script.js
@@ -18,15 +18,54 @@ let computerScore = 0;
 
 function getRoundResults(userOption) {
     const computerResult = getRandomComputerResult();
-    if (computerResult === userOption) {
-        playerScore += 0;
-        computerScore += 0;
-        return `It's a tie! Both chose ${userOption}.`;
-    }
+
     if (hasPlayerWonTheRound(userOption, computerResult)) {
-        playerScore += 1;
-        return `You won! ${userOption} beats ${computerResult}.`;
+        playerScore++;
+        return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if (computerResult === userOption) {
+        return `It's a tie! Both chose ${userOption}`;
+    } else {
+        computerScore++;
+        return `Computer wins! ${computerResult} beats ${userOption}`;
     }
-    computerScore += 1;
-    return `You lost! ${computerResult} beats ${userOption}.`;
 }
+
+const playerScoreSpanElement = document.getElementById('player-score');
+const computerScoreSpanElement = document.getElementById('computer-score');
+const roundResultsMsg = document.getElementById('results-msg');
+
+function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
+
+    if (playerScore === 3) {
+        winnerMsgElement.innerText = "Player wins the game!";
+        optionsContainer.style.display = "none";
+    } else if (computerScore === 3) {
+        winnerMsgElement.innerText = "Computer wins the game!";
+        optionsContainer.style.display = "none";
+    }
+}
+
+const rockBtn = document.getElementById('rock-btn');
+const paperBtn = document.getElementById('paper-btn');
+const scissorsBtn = document.getElementById('scissors-btn');
+
+rockBtn.addEventListener('click', function () {
+    showResults('Rock');
+});
+
+paperBtn.addEventListener('click', function () {
+    showResults('Paper');
+});
+
+scissorsBtn.addEventListener('click', function () {
+    showResults('Scissors');
+});
+
+const winnerMsgElement = document.getElementById('winner-msg');
+
+const optionsContainer = document.querySelector('.options-container');
+
+const resetGameBtn = document.getElementById('reset-game-btn');
