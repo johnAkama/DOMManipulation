0fa22b0429bb93c7839a776a491cbe0bed0aa673
Kodjo Akama <johnakama32@gmail.com>
2026-02-11 16:43:41 +0000

    Adds accessible tablist UI and improves code consistency
    Introduces a new tablist interface for planet facts using semantic HTML and ARIA roles for better accessibility.
    
    Refactors team stats code for improved readability and consistent formatting.
    
    Enhances overall user experience by making navigation clearer and code easier to maintain.

--- teamCards/script.js
+++ teamCards/script.js
@@ -1,13 +1,13 @@
 const footballTeam = {
-  team: 'John Mehner',
-  year: 1967,
-  headCoach: 'Kofie Desmel',
-  players: [
-    { name: 'John', position: 'midfielder', isCaptain: true },
-    { name: 'Jane', position: 'defender', isCaptain: false },
-    { name: 'Bob', position: 'forward', isCaptain: false },
-    { name: 'Alice', position: 'goalkeeper', isCaptain: false },
-  ],
+    team: 'John Mehner',
+    year: 1967,
+    headCoach: 'Kofie Desmel',
+    players: [
+        { name: 'John', position: 'midfielder', isCaptain: true },
+        { name: 'Jane', position: 'defender', isCaptain: false },
+        { name: 'Bob', position: 'forward', isCaptain: false },
+        { name: 'Alice', position: 'goalkeeper', isCaptain: false },
+    ],
 };
 
 // Display team info
@@ -19,13 +19,13 @@ const playersList = document.getElementById('player-cards');
 
 // Function to display a player's info
 function displayPlayer(player) {
-  const listItem = document.createElement('div');
-  listItem.classList.add('player-card');
-  listItem.innerHTML = `
+    const listItem = document.createElement('div');
+    listItem.classList.add('player-card');
+    listItem.innerHTML = `
     <h2>${player.name} ${player.isCaptain ? '(Captain)' : ''}</h2>
     <p>Position: ${player.position}</p>
   `;
-  playersList.append(listItem);
+    playersList.append(listItem);
 }
 
 // Display all players initially
@@ -33,13 +33,16 @@ footballTeam.players.forEach(displayPlayer);
 
 // Event listener for filtering players
 document.getElementById('players').addEventListener('change', (e) => {
-  playersList.innerHTML = ""; // Clear current list
-  const selectedPosition = e.target.value;
+    playersList.innerHTML = ''; // Clear current list
+    const selectedPosition = e.target.value;
 
-  // Display players based on the selected position
-  footballTeam.players.forEach((player) => {
-    if (selectedPosition === 'all' || player.position === selectedPosition) {
-      displayPlayer(player);
-    }
-  });
+    // Display players based on the selected position
+    footballTeam.players.forEach((player) => {
+        if (
+            selectedPosition === 'all' ||
+            player.position === selectedPosition
+        ) {
+            displayPlayer(player);
+        }
+    });
 });
