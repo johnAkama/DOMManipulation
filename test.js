0d7149eaa72ca852fc4d47fd67fd84aaa2020206
Kodjo Akama <johnakama32@gmail.com>
2026-02-19 14:43:44 +0000

    progress update

---
+++ test.js
@@ -0,0 +1,13 @@
+const developerObj = [
+    {
+        name: 'Jessica',
+        country: 'USA',
+        isEmployed: true,
+    },
+];
+
+developerObj.map((item, index, array) => {
+    console.log(
+        'This will throw an error because developerObj is not an array',
+    );
+});
