946f45015d496d48ce88f7ba6527f817acc0c9e0
Kodjo Akama <johnakama32@gmail.com>
2026-02-26 13:44:32 +0000

    progress update

--- test.js
+++ test.js
@@ -11,9 +11,12 @@ const developerObj = [
     },
 ];
 
-const testName = 'Jessica is a girl';
-const regEx = /Jessica/;
-console.log(testName.match(regEx));
+/* const testName = 'jessica is a girl';
+const testName2 = 'jesSiCA';
+const regEx = /jessica/gi;
+console.log(regEx.test(testName)); // Reset the lastIndex to search from the beginning
+console.log(regEx.lastIndex);
+console.log(regEx.test(testName2)); */
 
 /* function validateNumber(input) {
     if (typeof input !== 'number') {
@@ -28,3 +31,32 @@ try {
     console.error('An error occurred' + error.message);
 }
  */
+
+/* const start = /^freecodecamp/im;
+const end = /freecodecamp$/im;
+const string = `I really love
+freecodecamp
+it's my favorite`;
+console.log(start.test(string)); // true
+console.log(end.test(string)); // true
+ */
+
+/* const regex = /freecodecamp/im;
+const string = "we love freecodecamp isn't freecodecamp great?";
+console.log(string.match(regex));
+ */
+
+/* const regex = /freecodecamp/i;
+console.log(regex.test("I love FREECODECAMPINGSEE!"));
+console.log(regex.test("freeCodoCamp"));
+console.log(regex.test("We went freeCodeCamping.")); */
+
+const pattern = 'freecodecamp';
+const str = 'freecodecamp is the best we love freecodecamp';
+const matched = str.matchAll(pattern);
+const replaced = str.replaceAll(pattern, 'freeCodeCamp');
+console.log(matched);
+console.log(replaced);
+console.log(matched.next()); 
+console.log(matched.next()); 
+console.log(matched.next()); 
