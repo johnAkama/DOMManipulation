74ea2019018b6eb31225b5d9b9662339a55a8ca4
Kodjo Akama <johnakama32@gmail.com>
2026-03-06 13:59:59 +0000

    progress update

--- spamChecker/script.js
+++ spamChecker/script.js
@@ -2,11 +2,11 @@ const messageInput = document.getElementById('message-input');
 const result = document.getElementById('result-message');
 const checkMessageButton = document.getElementById('check-message-btn');
 
-const helpRegex = /please help/i;
+const helpRegex = /please help|assist me/i;
 
-const isSpam = (msg) => {
-    return msg.match(helpRegex);
-};
+const denyList = [helpRegex];
+
+const isSpam = (msg) => helpRegex.test(msg);
 
 checkMessageButton.addEventListener('click', () => {
     if (messageInput.value === '') {
