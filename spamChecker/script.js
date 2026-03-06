cb12d63e9dca86248748ca7cbd24cc077ae87b18
Kodjo Akama <johnakama32@gmail.com>
2026-03-06 11:26:18 +0000

    progress update

--- spamChecker/script.js
+++ spamChecker/script.js
@@ -1,7 +1,19 @@
-'user strict';
-
 const messageInput = document.getElementById('message-input');
-
 const result = document.getElementById('result-message');
-
 const checkMessageButton = document.getElementById('check-message-btn');
+
+const helpRegex = 'please help';
+
+const isSpam = (msg) => false;
+
+checkMessageButton.addEventListener('click', () => {
+    if (messageInput.value === '') {
+        alert('Please enter a message.');
+        return;
+    }
+
+    result.textContent = isSpam(messageInput.value)
+        ? 'Oh no! This looks like a spam message.'
+        : 'This message does not seem to contain any spam.';
+    messageInput.value = '';
+});
