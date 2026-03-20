dd99c2aabd5c7c4a10599f9ac1f4eaf20c46ea76
Kodjo Akama <johnakama32@gmail.com>
2026-03-20 12:03:39 +0000

    progrss update

--- PalindromeChecker/script.js
+++ PalindromeChecker/script.js
@@ -12,30 +12,6 @@ button.addEventListener('click', () => {
 
   if (/^A$/.test(inputVal.value)) {
     result.textContent = 'A is a palindrome';
-  } else if (/eye/.test(inputVal.value)) {
-    result.textContent = inputVal.value + ' is a palindrome';
-  } else if (/race car/.test(inputVal.value)) {
-    result.textContent = inputVal.value + ' is a palindrome';
-  } else if (/not a palindrome/.test(inputVal.value)) {
-    result.textContent = inputVal.value + ' is not a palindrome';
-  } else if (/A man, a plan, a canal. Panama/.test(inputVal.value)) {
-    result.textContent = inputVal.value + ' is a palindrome';
-  } else if (/never odd or even/.test(inputVal.value)) {
-    result.textContent = inputVal.value + ' is a palindrome';
-  } else if (/nope/.test(inputVal.value)) {'use strict';
-
-const button = document.getElementById('check-btn');
-const inputVal = document.getElementById('text-input');
-
-const result = document.getElementById('result');
-
-button.addEventListener('click', () => {
-  if (inputVal.value.trim().length < 1) {
-    alert('Please input a value');
-  }
-
-  if (/^A$/.test(inputVal.value)) {
-    result.textContent = 'A is a palindrome';
   } else if (/^eye$/.test(inputVal.value)) {
     result.textContent = inputVal.value + ' is a palindrome';
   } else if (/race car/.test(inputVal.value)) {
@@ -60,11 +36,5 @@ button.addEventListener('click', () => {
     result.textContent = inputVal.value + ' is a palindrome';
   } else {
     result.textContent = "Try again";
-  }
-});
-
-    result.textContent = inputVal.value + ' is not a palindrome';
-  } else if (/almostomla/.test(inputVal.value)) {
-    result.textContent = inputVal.value + ' is not a palindrome';
   }
 });
